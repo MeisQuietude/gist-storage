7ac8ab926870094a104d1444f16633403578e28c
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-12 22:56:13 +0500

    page for create gists, done append and delete of fragments

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('layout.html')
+    return render_template('gist-create.html')
 
 
 if __name__ == '__main__':
---
+++ static/js/gist-create.js
@@ -0,0 +1,12 @@
+const gists = document.querySelector('#gists');
+const gist = document.querySelector('.gist');
+
+const addBtn = document.querySelector('#addBtn');
+addBtn.onclick = () => {
+    const gistCloned = gist.cloneNode(true);
+    gistCloned.querySelector('.delBtn').onclick = (e) => {
+        const el = e.target.parentNode;
+        el.parentNode.removeChild(el);
+    };
+    gists.appendChild(gistCloned);
+};
