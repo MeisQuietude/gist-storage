233d8316aa9b7bdc4a59edf9a8cdf8b2b5b7f6a0
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-13 00:03:07 +0500

    structured

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,7 +6,24 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('gist-create.html')
+    return render_template('gist/create.html')
+
+
+@app.route('/discover')
+def discover():
+    return render_template('gist/list.html')
+
+
+@app.route('/gist/<id>', methods=['GET', 'POST'])
+def gist(id):
+    """
+    GET: looking for page of gist
+    POST: create new one gist
+    """
+    if request.method == 'POST':
+        pass
+    else:
+        pass
 
 
 if __name__ == '__main__':
