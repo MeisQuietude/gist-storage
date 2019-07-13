514c8c0f1ca1406923dfc987a9c30a9de6c4774a
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-13 23:59:26 +0500

    model structure moved to another module, method create_app(), db create, context

--- app.py
+++ app.py
@@ -3,69 +3,32 @@ import re
 
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
 from werkzeug.datastructures import ImmutableMultiDict
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'jdbc:postgresql://localhost:5432/gists'
+from config import SQLALCHEMY_DATABASE_URL
+
 db = SQLAlchemy()
 
 
-class DB_Tools:
-    @staticmethod
-    def gen_id():
-        # TODO
-        return 0
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URL
+    db.init_app(app)
 
-    @staticmethod
-    def make_link():
-        # TODO
-        return "no"
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    if not database_exists(engine.url):
+        create_database(engine.url)
+    with app.app_context():
+        from model import Gist, Snippet
+        db.create_all()
+        db.session.commit()
 
-
-class Gist(db.Model):
-    id_ = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(255))
-    is_private = db.Column(db.Boolean)
-    link = db.Column(db.String(80))
-
-    # For __repr__
-    columns = ('id_', 'description', 'is_private', 'link')
-    values = (id_, description, is_private, link)
-
-    def __init__(self, description, is_private):
-        self.description = description
-        self.is_private = is_private
-        self.id_ = DB_Tools.gen_id()
-        self.link = DB_Tools.make_link()
-
-    def __repr__(self):
-        """
-        :return: dict {"column_name": column_value}
-        """
-        return {k: v for k, v in zip(self.columns, self.values)}
+    return app
 
 
-class Snippet(db.Model):
-    id_ = db.Column(db.Integer, primary_key=True)
-    gist_id = db.Column(db.Integer)
-    language = db.Column(db.String(80))
-    code = db.Column(db.String)
-
-    # For __repr__
-    columns = ('id_', 'gist_id', 'language', 'code')
-    values = (id_, gist_id, language, code)
-
-    def __init__(self, gist_id, language, code):
-        self.id_ = DB_Tools.gen_id()
-        self.gist_id = gist_id
-        self.language = language
-        self.code = code
-
-    def __repr__(self):
-        """
-        :return: dict {"column_name": column_value}
-        """
-        return {k: v for k, v in zip(self.columns, self.values)}
+app = create_app()
 
 
 @app.route('/', methods=['GET', 'POST'])
