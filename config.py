52730b0d885c24a64e74f369cf566f78a64b990b
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-14 22:08:36 +0500

    app config options added

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
-APP_DEBUG: bool = True
 POSTGRES_DATABASE_URL: str = 'postgresql://root:toor@localhost:5432/gists'
+
+# Code config
+MAX_NUMBER_LINES_PREVIEW = 10
+MAX_NUMBER_SYMBOLS_IN_LINE_PREVIEW = 80
