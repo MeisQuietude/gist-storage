f5328d022338edb9ec2f45fc845b9c10ac8d47b1
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-14 12:27:48 +0500

    implemented: gen_id(), mk_link(); is_private -> is_public

--- model.py
+++ model.py
@@ -1,34 +1,33 @@
+import uuid
 from app import db
 
 
 class DB_Tools:
     @staticmethod
     def gen_id():
-        # TODO
-        return 0
+        return uuid.uuid4().hex
 
     @staticmethod
-    def make_link():
-        # TODO
-        return "no"
+    def make_link(id_: str, is_public: bool):
+        return id_[:9] if is_public else id_[9:]
 
 
 class Gist(db.Model):
     id_ = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(255), nullable=False)
-    is_private = db.Column(db.Boolean)
+    is_public = db.Column(db.Boolean)
     link = db.Column(db.String(80), unique=True)
     snippets = db.relationship('Snippet', backref="gist", lazy="dynamic")
 
     # For __repr__
-    columns = ('id_', 'description', 'is_private', 'link')
-    values = (id_, description, is_private, link)
+    columns = ('id_', 'description', 'is_public', 'link')
+    values = (id_, description, is_public, link)
 
-    def __init__(self, description, is_private):
+    def __init__(self, description, is_public=False):
         self.description = description
-        self.is_private = is_private
+        self.is_public = is_public
         self.id_ = DB_Tools.gen_id()
-        self.link = DB_Tools.make_link()
+        self.link = DB_Tools.make_link(self.id_, self.is_public)
 
     def __repr__(self):
         return f'Gist {self.id_}'
