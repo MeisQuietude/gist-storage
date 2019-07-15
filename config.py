114cbd0904916915c250e4cccc0bb2662793211a
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-15 20:28:18 +0500

    added option max number of gists on page

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ POSTGRES_DATABASE_URL: str = 'postgresql://root:toor@localhost:5432/gists'
 # Code config
 MAX_NUMBER_LINES_PREVIEW = 10
 MAX_NUMBER_SYMBOLS_IN_LINE_PREVIEW = 80
+
+NUMBER_GISTS_ON_PAGE = 5
