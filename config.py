08825aa3bce4762be4477b9d26dbb0dd0b3f8dec
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-20 03:07:47 +0500

    added supported languages dictionary (name:{exts}}

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 POSTGRES_DATABASE_URL: str = 'postgresql://root:toor@localhost:5432/gists'
+SUPPORTED_LANGUAGES = {
+    None: {},
+    'javascript': {'.js'},
+    'python': {'.py'},
+    'c++': {'.cpp'},
+    'php': {'.myphp'},
+    'html': {'htm', 'html'}
+}
 
-# Code config
+# Options
 MAX_NUMBER_LINES_PREVIEW = 10
 MAX_NUMBER_SYMBOLS_IN_LINE_PREVIEW = 80
 
