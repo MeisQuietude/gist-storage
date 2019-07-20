a10e56613fde585d92f0df38d77bfc15da3a1760
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-20 23:35:18 +0500

    fixes docstring

--- upload_file.py
+++ upload_file.py
@@ -15,9 +15,9 @@ class SizeException(BaseException):
 def download_file_by_url(url: str, max_size: int = None, retry: int = 0):
     """
     Скачивает файл по ссылке
-    :param url:
-    :param ограничение на размер файла, смотрятся по заголовкам max_size:
-    :param количество попыток получить 200 ответ от сервера retry:
+    :param url
+    :param max_size: ограничение на размер файла, смотрятся по заголовкам max_size
+    :param retry: количество попыток получить 200 ответ от сервера retry
     :return: dict{filename : content}
     """
     request_params = {
@@ -30,16 +30,16 @@ def download_file_by_url(url: str, max_size: int = None, retry: int = 0):
         raise DownloadException(response.content)
     if max_size and not _check_size(url, max_size):
         return SizeException(url)
-    file_name = url.split('/')[-1]
-    return json.dumps({'filename': file_name, 'content': response.content.decode('utf-8')})
+    filename = url.split('/')[-1]
+    return json.dumps({'filename': filename, 'content': response.content.decode('utf-8')})
 
 
 def _wait_response(request_params: dict, retry: int) -> requests.Response:
     """
-    Ждет 200 ответа от сервера
+    Wait 200 answer from server
     :param request_params:
     :param retry:
-    :return: :class:`Response <Response>` object
+    :return: class:`Response <Response>` object
     :rtype: requests.Response[
     """
     response = requests.get(**request_params)
@@ -52,7 +52,7 @@ def _wait_response(request_params: dict, retry: int) -> requests.Response:
 
 def _check_size(url: str, max_size: int) -> bool:
     """
-    Провяряет заголовки на допустимый размер контента
+    Checks headers for allowed size of content
     :param url:
     :param max_size:
     :return:
@@ -63,9 +63,3 @@ def _check_size(url: str, max_size: int) -> bool:
     if content_length and content_length > max_size:
         return False
     return True
-
-
-if __name__ == '__main__':
-    uri = 'https://raw.githubusercontent.com/odoo/odoo/12.0/odoo/modules/graph.py'
-    f = download_file_by_url(uri)
-    print(f)
