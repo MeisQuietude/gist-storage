99015c37027382719233d83df22281b5b863aeeb
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-20 23:52:49 +0500

    added url validation

--- static/js/upload-file.js
+++ static/js/upload-file.js
@@ -36,6 +36,8 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
         };
 
         const url = await prompt('Enter your url');
+        if (!isValidURL(url)) return alert('URL is not valid!');
+
         const {filename, content} = await getRequest(url);
 
         filenameArea.value = filename;
@@ -46,3 +48,7 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
     fileInput.disable();
     urlInput.disable();
 }
+
+const isValidURL = url =>
+    /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i
+        .test(url);
