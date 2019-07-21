f596a9970606f20c008ce47b4e61ec2d13a423b1
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-21 20:46:10 +0500

    flask optimize

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_DATABASE_URL
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     engine = create_engine(POSTGRES_DATABASE_URL)
