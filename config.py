37500b8c6c3851c78031f0c28cb76291a51d9cf0
Quietude <savelyev_stepan@mail.ru>
2019-07-24 15:23:38 +0500

    added config classes, changed connection url type

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-POSTGRES_DATABASE_URL: str = 'postgresql://root:toor@localhost:5432/gists'
 SUPPORTED_LANGUAGES = {
     None: {},
     'javascript': {'.js'},
@@ -14,3 +13,25 @@ MAX_NUMBER_SYMBOLS_IN_LINE_PREVIEW = 80
 
 NUMBER_GISTS_ON_PAGE = 5
 COUNT_VIEW_PAGE_NUMBERS = 3  # +- from current page
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    DB_USER = 'gist'
+    DB_PASS = 'gist'
+
+
+class ProductionConfig(Config):
+    pass
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI: str = f'postgresql+psycopg2://{Config.DB_USER}:{Config.DB_PASS}@localhost:5432/gists'
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
