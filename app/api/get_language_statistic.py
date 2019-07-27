fa54e80a9f7bb95f761a3c3d41deee773b7d7c97
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-27 15:19:11 +0500

    added docstring

--- app/api/get_language_statistic.py
+++ app/api/get_language_statistic.py
@@ -4,6 +4,10 @@ from app.models.gist import Gist
 
 
 def get_language_statistic():
+    """
+    Collect statistic from snippets by language usage
+    :return: sorted by language names dictionary with fractional values
+    """
     count = defaultdict(int)  # dictionary {'language': default(0)}
     for gist in Gist.query.filter(Gist.is_public):
         for snippet in gist.snippets:
@@ -12,7 +16,7 @@ def get_language_statistic():
     total_count = sum(count.values())  # count all snippets
     stats = OrderedDict(sorted(count.items(), key=lambda lang: lang[0].__repr__()))
     for k in stats.keys():
-        # get a percent values
+        # get a fractional values (for percent calculating)
         stats[k] = stats[k] / total_count
 
     return stats
