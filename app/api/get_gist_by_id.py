e22e788812527a840ef4daddd101c74b20cadb18
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-27 15:08:26 +0500

    rewrote to more readable

--- app/api/get_gist_by_id.py
+++ app/api/get_gist_by_id.py
@@ -6,21 +6,21 @@ def get_gist(id_) -> Gist or None:
     :param id_: id_ (len:32), public link (len:8), private link (len:24)
     :return: object (type 'Gist') or None
     """
-    try:
-        assert id_ is not None
 
-        id_length = len(id_)
-        if id_length == 32:
-            # Full id\link ( developer case )
-            result = Gist.query.filter(Gist.id_ == id_).first()
-        elif id_length == 24:
-            # Private link
-            result = Gist.query.filter(Gist.id_.endswith(id_)).first()
-        elif id_length == 8:
-            # Public link
-            result = Gist.query.filter(Gist.id_.startswith(id_)).first()
-        else:
-            raise AssertionError
-        return result
-    except AssertionError:
+    if id_ is None:
         return None
+
+    result = None
+
+    id_length = len(id_)
+    if id_length == 32:
+        # Full id\link ( developer case )
+        result = Gist.query.filter(Gist.id_ == id_).first()
+    elif id_length == 24:
+        # Private link
+        result = Gist.query.filter(Gist.id_.endswith(id_)).first()
+    elif id_length == 8:
+        # Public link
+        result = Gist.query.filter(Gist.id_.startswith(id_)).first()
+
+    return result
