04d50345a346849363047fc08edab87002406400
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-27 18:16:46 +0500

    docstring

--- app/api/front/count_pages.py
+++ app/api/front/count_pages.py
@@ -1,7 +1,13 @@
 from math import ceil
 
+from app.models.gist import Gist
 from config import NUMBER_GISTS_ON_PAGE
 
 
-def count_pages(all_gists):
-    return ceil(len(all_gists) / NUMBER_GISTS_ON_PAGE)
+def count_pages() -> int:
+    """
+    Count pages
+    :return: number of pages
+    """
+    gists = Gist.query.filter(Gist.is_public).all()
+    return ceil(len(gists) / NUMBER_GISTS_ON_PAGE)
