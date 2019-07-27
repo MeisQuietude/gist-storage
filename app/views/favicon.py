b28888e96ed23d89f54addf62128b65aec473e8c
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-27 18:48:13 +0500

    fix favicon

--- app/views/favicon.py
+++ app/views/favicon.py
@@ -4,4 +4,4 @@ from flask import send_from_directory
 
 
 def favicon(root):
-    return send_from_directory(os.path.join(root, 'assets'), 'favicon.ico')
+    return send_from_directory(os.path.join(root, 'static', 'images'), 'favicon.ico')
