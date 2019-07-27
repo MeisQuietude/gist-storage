d0720b48c797a960992d1d5f71d1fdf6fd448e5d
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-27 15:15:47 +0500

    added docstring

--- app/api/get_file_by_url.py
+++ app/api/get_file_by_url.py
@@ -3,4 +3,9 @@ from config import MAX_SIZE_UPLOAD_CONTENT_BY_URL
 
 
 def get_file_by_url(url):
+    """
+    Upload content by scrapping url
+    :param url:
+    :return: json.dumps({filename: filename, content: content})
+    """
     return get_file(url, max_size=MAX_SIZE_UPLOAD_CONTENT_BY_URL)
