968845136b3e822c12190504b2be295a096d4e9e
Stepan Savelyev <savelyev_stepan@mail.ru>
2019-07-27 18:17:58 +0500

    added docstring

--- app/api/guess_language_by_extension.py
+++ app/api/guess_language_by_extension.py
@@ -2,6 +2,11 @@ from config import SUPPORTED_LANGUAGES
 
 
 def guess_language_by_extension(ext: str) -> int:
+    """
+    Try to get language id by file extension
+    :param ext: file extension (like '.ext')
+    :return: language_id
+    """
     ext = ext.lower()
     for supp_lang, supp_ext in SUPPORTED_LANGUAGES.items():
         if ext in supp_ext:
